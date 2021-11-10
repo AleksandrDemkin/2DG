3d551a4720bed34bcd870e2cb8870cf56f026730
Aleksandr <djomadjomich@gmail.com>
2021-11-11 00:56:06 +0200

    Task1

---
+++ Task1/Assets/Configs/Resources/SpriteAnimationsConfig.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Track
+{
+    sonic_walk,
+    sonic_jump
+    //add new tracks here
+} 
+
+[CreateAssetMenu(fileName = "SpriteAnimationsConfig", menuName =
+        "Configs/SpriteAnimationsConfig", order = 1)]
+public class SpriteAnimationsConfig : ScriptableObject
+{
+    [Serializable]
+    public class SpritesSequence
+    {
+        public Track Track;
+        public List<Sprite> Sprites = new List<Sprite>();
+    }
+    public List<SpritesSequence> Sequences = new List<SpritesSequence>();
+
+}
---
+++ Task1/Assets/Scripts/Views/CharacterView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike
+{
+    public class CharacterView : MonoBehaviour
+    {
+        public SpriteRenderer SpriteRenderer;
+        //add other useful refs
+    }
+}
